72c151b6b766514644eb5cb1780489dc6d1549bc
ashwin <ashwin5devarajan@gmail.com>
2015-02-01 08:43:19 +0530

    cloud-html

---
+++ codexdelta/cloud/cloud9.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>CodeDelta</title> 
+  <meta content="width=device-width; initial-scale=1; minimum-scale=1; maximum-scale=1; user-scalable=no;" name="viewport">
+  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <!--[if lt IE 9]><script src="js/respond.min.js"></script><![endif]-->
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="main">
+    <!--<header>
+      <a href="#"><img src="img/back_arrow.png" alt="Back" /></a>
+    </header>-->
+    <section>
+      <div id="top">
+        <h1>Casual Mens Shirt : BlackBerry</h1>
+        <div id="ratings">
+                       <div class="star-wrapper"><img class="stars" src="img/star-red.png" alt="Star" /></div>
+                       <div class="star-wrapper"><img class="stars" src="img/star-red.png" alt="Star" /></div>
+                       <div class="star-wrapper"><img class="stars" src="img/star-red.png" alt="Star" /></div>
+                       <div class="star-wrapper"><img class="stars" src="img/star-red.png" alt="Star" /></div>
+                                 <div class="star-wrapper"><img class="stars" src="img/star-gray.png" alt="Star" /></div>
+          
+          <div class="rating">(41)</div>              
+        </div>
+         <div id="book">
+          <img src="img/cloud9.jpg" alt="Cloud Recognition in Vuforia - Book" />
+        </div>
+      </div>
+      <div id="bottom">
+        <div id="info">
+          <div id="author"><span>Brand: </span>BlackBerry</div>
+          <div class="price"><span>List Price: </span><span class="second line-through">$43.99</span></div>
+          <div class="price"><span>Your Price: </span>$43.00</div>
+          <div class="price"><span>You Save: </span>$0.84 (6%)</div>
+        </div>
+      </div>
+    </section>
+    <footer>CodexDelta: Prototype</footer>
+  </div>
+</body>
+</html>
